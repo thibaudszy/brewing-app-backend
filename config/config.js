e44ce25ebedd09545c90cb8438ac65ea0b4a269a
thibaudszy <thivaudszy@gmail.com>
2020-11-25 14:33:08 +0100

    removes the config.json

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config(); // we configure dotenv
+module.exports = {
+  development: {
+    url: process.env.ELEPHANT_SQL, // and use the env vars to get the url
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
